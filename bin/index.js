a20df828981d7454a3e7e6a0765883749ba4c7d8
周伟滨 <zhouweibin@zhouweibindeMacBook-Pro.local>
2022-05-08 23:47:24 +0800

    chore: 输出调整

--- bin/index.js
+++ bin/index.js
@@ -3,7 +3,6 @@
 const program = require("commander");
 const chalk = require("chalk");
 const updateChk = require("../lib/update");
-const dlTemplate = require("../lib/download");
 const initProject = require("../lib/init");
 
 // 获取版本号
@@ -12,35 +11,26 @@ program.version(require("../package.json").version, "-v, --version");
 // 检查更新
 program
   .command("upgrade")
-  .description("Check the js-pro-cli version.")
+  .description("Check the jupiter version.")
   .action(() => {
     updateChk();
   });
 
-program
-  // 监听 --help 执行
-  .on("--help", () => {
-    // 新增说明信息
-    console.log(
-      `\r\nRun ${chalk.cyan(
-        `zr <command> --help`
-      )} for detailed usage of given command\r\n`
-    );
-  });
-// template 下载/更新模板
-// program
-//   .command("template")
-//   .description("Download template.")
-//   .action(() => {
-//     dlTemplate();
-//   });
+program.on("--help", () => {
+  // 新增说明信息
+  console.log(
+    `\r\nRun ${chalk.cyan(
+      `zr <command> --help`
+    )} for detailed usage of given command\r\n`
+  );
+});
 
 // init 初始化项目
 program
-  .name("js-pro-cli")
+  .name("jupiter")
   .usage("<commands> [options]")
   .command("init <project_name>")
-  .description("Create a react project.")
+  .description("Create a project.")
   .action((project) => {
     initProject(project);
   });
